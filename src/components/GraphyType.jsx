43854142570b44a0f0f33e70d11832a0c3a3a3b7
Sebastián Zapateiro <zapateirosebastian@hotmail.com>
2023-04-26 11:11:24 -0500

    master: Top structure design created

---
+++ src/components/GraphyType.jsx
@@ -0,0 +1,26 @@
+export function GraphyType() {
+    return (
+        <div>
+
+            <p>Predice tu prueba saber pro</p>
+            <div>
+                <div>
+                    <h1>
+                        Ingresa para saber tu <br/>
+                        estado de aprobación
+                    </h1>
+                </div>
+                <div>
+                    <p>Conoce</p>
+                    <hr></hr>
+                    <ol>
+                        <li>Nivel de desempeño</li>
+                        <li>Promedio de puntaje</li>
+                        <li>Precentil global</li>
+                        <li>Estado actual graficamente</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    )
+}
